e95593175d8320246ff1c2bd99530866b1071523
talat <talatcan.ayhan@gmail.com>
2023-11-08 13:34:34 +0300

    Completed the tasks

--- etchasketch.js
+++ etchasketch.js
@@ -1,29 +1,56 @@
-const gridContainer = document.createElement("div");
-gridContainer.setAttribute("id", "grid-container");
-document.querySelector("body").appendChild(gridContainer);
+const body = document.querySelector("body");
 
+const btn = document.createElement("button");
+const gridRowColor = "brown";
 
+btn.innerHTML = "SET NEW GRID";
+btn.onclick = () => {
+    const newPixelNum = window.prompt("Enter the number of pixels");
+    createGrid(newPixelNum);
+};
+body.appendChild(btn);
 
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.setAttribute("class", "grid-row-${i+1}");
-    gridRow.style.display = "flex";
-
-    for (let j =0; j < 16; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.setAttribute("class", "grid-item");
-
-        //Set CSS style attributes for grid items// 
-        gridItem.style.height = "25px";
-        gridItem.style.width = "25px";
-        gridItem.style.border = "solid";
-        gridItem.onmouseover = () => {gridItem.style.backgroundColor = "yellow"};
-        gridItem.onmouseleave = () => {gridItem.style.backgroundColor = "white"};
-
-        
-
-        gridRow.appendChild(gridItem);
-    }
-
-    gridContainer.appendChild(gridRow);
+function createGridContainer() {
+    const gridContainer = document.createElement("div");
+    gridContainer.style.width = "500px";
+    gridContainer.style.height = "500px";
+    gridContainer.style.backgroundColor = "blue";
+    gridContainer.setAttribute("id", "grid-container");
+    body.appendChild(gridContainer);
+    return gridContainer;
 }
+function clearBody() {
+    const removed = document.getElementById("grid-container");
+    if (removed != null)
+        removed.remove();
+}
+
+function createGrid(pixelNum) {
+    clearBody();
+    createGridContainer();
+
+    for (let i = 0; i < pixelNum; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row-${i+1}");
+
+        gridRow.style.backgroundColor = gridRowColor;
+        gridRow.style.display = "flex";
+
+        for (let j = 0; j < pixelNum; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.setAttribute("class", "grid-item");
+            //Set CSS style attributes for grid items// 
+            gridItem.style.width = (500 / pixelNum) + "px";
+            gridItem.style.height = (500 / pixelNum) + "px";
+            gridItem.style.border = "solid";
+            gridItem.style.borderWidth = "1px";
+            gridItem.onmouseover = () => {gridItem.style.backgroundColor = "yellow"};
+            //gridItem.onmouseleave = () => {gridItem.style.backgroundColor = gridRowColor};
+
+            gridRow.appendChild(gridItem);
+        }
+        document.getElementById("grid-container").appendChild(gridRow);
+    }
+}
+
+createGrid(16);
